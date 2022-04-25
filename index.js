fd0bcce9ad69a14b74ffe7627c850156dc4efd7c
anirudh <anirudhece2020@gmail.com>
2022-04-25 17:48:03 +0530

    final updates

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 let btn= document.getElementById('button');
-
 let bulb = document.getElementById('bulb-btn');
-
 btn.addEventListener('click',togglebulb);
 
 function togglebulb()
 {
     if(btn.textContent.includes('ON') )
     {
-        bulb.src="images\bulb-on.jpg";
+        bulb.src="images/bulb-on.jpg";
         btn.textContent="OFF";
     }
     else if(btn.textContent.includes('OFF') )
     {
-        bulb.src="images\bulb-off.jpg";
+        bulb.src="images/bulb-off.jpg";
         btn.textContent="ON";
     }
 }
